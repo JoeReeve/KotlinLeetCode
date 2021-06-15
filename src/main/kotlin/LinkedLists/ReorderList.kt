72dea2b59e9882daf29d66c4f2337ef16028e15c
Joe Reeve <jreeve36@gmail.com>
2021-06-14 16:37:39 -1000

    added 'ReorderList' to the linked list class

---
+++ src/main/kotlin/LinkedLists/ReorderList.kt
@@ -0,0 +1,50 @@
+package LinkedLists
+
+import Node
+
+class ReorderList {
+
+    fun reorderList(head: Node?) {
+        if (head == null || head.next == null) return
+        val l1: Node = head
+        var slow = head
+        var fast = head
+        var prev: Node? = null
+        while (fast != null && fast.next != null) {
+            prev = slow
+            slow = slow!!.next
+            fast = fast.next!!.next
+        }
+        prev!!.next = null
+        val l2 = reverse(slow)
+        merge(l1, l2)
+    }
+
+    fun reverse(head: Node?): Node? {
+        var prev: Node? = null
+        var curr = head
+        var next = head
+        while (curr != null) {
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        return prev
+    }
+
+    fun merge(l1: Node?, l2: Node?) {
+        var l1 = l1
+        var l2 = l2
+        while (l1 != null) {
+            val next1 = l1.next
+            val next2 = l2!!.next
+            l1.next = l2
+            if (next1 == null) break
+            l2.next = next1
+            l1 = next1
+            l2 = next2
+        }
+    }
+
+}
