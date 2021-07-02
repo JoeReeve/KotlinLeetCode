a27e6418c1b115aac9b15b5d2ff0457f52f34bd2
Joe Reeve <jreeve36@gmail.com>
2021-07-02 11:07:24 -1000

    Added bottum up solution to 'Fibonacci Numbers'

--- src/main/kotlin/DynamicProgramming/FibonacciSequence.kt
+++ src/main/kotlin/DynamicProgramming/FibonacciSequence.kt
@@ -3,7 +3,7 @@ package DynamicProgramming
 class FibonacciSequence {
 
     val memo = IntArray(31)
-    
+
     fun fib(num: Int): Int {
         if (num <= 1) return num
         if (memo[num] != 0) return memo[num]
@@ -12,6 +12,17 @@ class FibonacciSequence {
         memo[num] = result
         return result
     }
+
+    fun fibBottomUp(num: Int): Int {
+
+        val memo = IntArray(num + 1)
+        memo[1] = 1
+        memo[2] = 1
+        for (i in 3..num) {
+            memo[i] = memo[i - 1] + memo[i - 2]
+        }
+        return memo[num]
+    }
 }
 
 fun main() {
@@ -19,4 +30,5 @@ fun main() {
     var fib = FibonacciSequence()
 
     println(fib.fib(5))
+    println(fib.fibBottomUp(5))
 }
