2b033786535c05e81c7db7dcb0cebe2ddd8e5e65
Joe Reeve <jreeve36@gmail.com>
2021-07-15 18:10:18 -1000

    Added solution to 'Unique Paths'

--- src/main/kotlin/DynamicProgramming/DecodeWays.kt
+++ src/main/kotlin/DynamicProgramming/DecodeWays.kt
@@ -3,7 +3,7 @@ package DynamicProgramming
 class DecodeWays {
 
     fun numDecodings(s: String): Int {
-        val dp = IntArray(s.length + 1) {0}
+        val dp = IntArray(s.length + 1)
         dp[0] = 1
         dp[1] = if (s[0] == '0') 0 else 1
         for (i in 2..s.length) {
@@ -12,7 +12,7 @@ class DecodeWays {
             if (oneDigit >= 1) dp[i] += dp[i-1]
             if (twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i-2]
         }
-        return dp[s.length]
+        return dp.last()
     }
-    
+
 }
