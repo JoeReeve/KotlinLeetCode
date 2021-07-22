d00f7858f51785cbcb3ebae0f9bf89083ae48fa7
Joe Reeve <jreeve36@gmail.com>
2021-07-21 18:13:56 -1000

    Added solution to 'Reverse Binary Tree'

---
+++ src/main/kotlin/Trees/InvertBinaryTree.kt
@@ -0,0 +1,16 @@
+package Trees
+
+class InvertBinaryTree {
+
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root ==  null) return null
+
+        val left = invertTree(root.left)
+        val right = invertTree(root.right)
+        root.left = right
+        root.right = left
+
+        return root
+    }
+
+}
