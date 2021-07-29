447bd0f90c8b9f4a9a7210fa9ccfc5068d8364de
Joe Reeve <jreeve36@gmail.com>
2021-07-28 19:41:40 -1000

    Added BFS to binary tree class

---
+++ src/main/kotlin/Trees/BinarySearchTree.kt
@@ -0,0 +1,69 @@
+package Trees
+
+import java.util.*
+
+class BinarySearchTree(var root: TreeNode) {
+
+    fun insert(node: TreeNode) {
+        if (root == null) root = node
+        var curr = node
+        while (true) {
+            if (node.`val` < curr.`val`) {
+                if (curr.left == null) {
+                    curr.left = node
+                    return
+                } else curr = curr.left!!
+            } else {
+                if (curr.right == null) {
+                    curr.right = node
+                    return
+                } else curr = curr.right!!
+            }
+        }
+    }
+    
+    fun interativeSearch(value: Int):TreeNode? {
+        if (root == null) return null
+        var curr = root
+        while (curr != null) {
+            if (value < curr.`val`) curr = curr.left!!
+            else if (value > curr.`val`) curr = curr.right!!
+            else return curr
+        }
+        return root
+    }
+
+    fun recursiveSearch(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) return null
+        if (root.`val` == `val`) return root
+
+        if (root.`val` < `val`) return recursiveSearch(root.right, `val`)
+        else return recursiveSearch(root.left, `val`)
+    }
+
+    fun breadthFirstSearch(): List<Int> {
+        var curr = root
+        var list = mutableListOf<Int>()
+        var queue = LinkedList<TreeNode>()
+        queue.add(curr)
+
+        while (queue.size > 0) {
+            curr = queue.removeFirst()
+            list.add(curr.`val`)
+            if (curr.left != null) queue.add(curr.left!!)
+            if (curr.right != null) queue.add(curr.right!!)
+        }
+        return list
+    }
+
+    fun breadthFirstSearchR(queue: LinkedList<TreeNode>, list: MutableList<Int>): List<Int> {
+        if (queue.size == 0) return list
+        var curr = queue.removeFirst()
+        list.add(curr.`val`)
+        if (curr.left != null) queue.add(curr.left!!)
+        if (curr.right != null) queue.add(curr.right!!)
+
+        return breadthFirstSearchR(queue, list)
+    }
+
+}
