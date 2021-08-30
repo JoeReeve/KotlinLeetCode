f7fba18da5890995f70f981f963c52d225266ca4
Joe Reeve <jreeve36@gmail.com>
2021-08-30 11:38:56 -1000

    Actually added solution to 'Non-Overlapping Intervals'

--- src/main/kotlin/Intervals/NonOverlappingIntervals.kt
+++ src/main/kotlin/Intervals/NonOverlappingIntervals.kt
@@ -1,4 +1,27 @@
 package Intervals
 
 class NonOverlappingIntervals {
+
+    fun eraseOverlapIntervals(intervals: Array<IntArray>): Int {
+        var intervals = intervals.sortedBy{it[0]}
+        var count = 0
+        var left = 0
+        var right = 1
+
+        while (right < intervals.size) {
+            if (intervals[left][1] <= intervals[right][0]) {
+                left = right
+                right++
+            } else if (intervals[left][1] <= intervals[right][1]) {
+                count++
+                right++
+            } else if (intervals[left][1] > intervals[right][1]) {
+                left = right
+                right++
+                count++
+            }
+        }
+        return count
+    }
+
 }
